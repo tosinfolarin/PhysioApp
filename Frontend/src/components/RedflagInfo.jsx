a52dd6809c24aa474e59014c8302f67f9c376cce
Theo <theo.osinfolarin@gmail.com>
2024-06-28 17:14:18 +0100

    frontpage editted and faq further edited

--- Frontend/src/components/RedflagInfo.jsx
+++ Frontend/src/components/RedflagInfo.jsx
@@ -1,11 +1,13 @@
 const RedFlagInfo = () => {
-    return ( 
-        <div>
-            <h2>Red flags are distinct indicators identified through medical history and clinical examination, 
-                typically associated with a heightened risk of serious conditions such as infection, cancer, or fractures. 
-                It is important these are not missed. Please check the tabs below before continuing with the exercises so that we can ensure these have not been missed.</h2>
-        </div>
-     );
+  return (
+    <div className="red-flag-info-container">
+      <h2>
+        Red flags are distinct indicators identified through medical history and clinical examination,
+        typically associated with a heightened risk of serious conditions such as infection, cancer, or fractures.
+        It is important these are not missed. Please check the tabs below before continuing with the exercises so that we can ensure these have not been missed.
+      </h2>
+    </div>
+  );
 }
- 
+
 export default RedFlagInfo;
--- Frontend/src/components/RedflagWarning.jsx
+++ Frontend/src/components/RedflagWarning.jsx
@@ -2,13 +2,14 @@ import PhysioConsultationImg from "../images/PhysioConsultationImg.png"; // Adju
 
 const RedflagWarning = () => {
   return (
-    <div className="aboutme-page">
+    <div className="red-flag-page">
         
-      <div className="pic-box">
-        <h1>Before You Continue, Please Read, It is Vital!</h1>
+      <div className="red-flag-intro">
+        <h1>Red flags are critical warning signs that indicate serious underlying health conditions. 
+          It is vital you read the information below before continuing with the exercises.</h1>
       </div>
 
-      <div className="flex-c aboutme" role="region" aria-label="About Me">
+      <div className="flex-c redflags" role="region" aria-label="About Me">
         <img src={PhysioConsultationImg} alt="Image of physiotherapy consultation" className="ConsultationPic"/>
       </div>
 
