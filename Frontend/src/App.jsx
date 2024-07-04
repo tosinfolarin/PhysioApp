7455b9b0cd378bdd8fd5abda5e9d310740d479ee
Theo <theo.osinfolarin@gmail.com>
2024-07-04 13:00:30 +0100

    neck exercises template complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,10 @@ import FAQs from './components/FAQPage';
 import MyProfile from './components/MyProfilePage';
 import SignIn from './components/SigningInPage';
 import SignUp from './components/SignUpPage';
+import NeckExercises from './components/NeckEx1';
+import NeckExercises2 from './components/NeckEx2';
+import NeckExercises3 from './components/NeckEx3';
+
 
 function App() {
   return (
@@ -17,6 +21,9 @@ function App() {
         <Route path="/MyProfile" element={<MyProfile/>} />
         <Route path="/Sign-In" element={<SignIn/>} />
         <Route path="/Sign-Up" element={<SignUp/>} />
+        <Route path="/neck-pain" element={<NeckExercises/>}/>
+        <Route path="/neck-pain/level-2" element={<NeckExercises2/>}/>
+        <Route path="/neck-pain/level-3" element={<NeckExercises3/>}/>
         
       </Routes>
     </Router>
---
+++ Frontend/src/components/NeckEx1.jsx
@@ -0,0 +1,39 @@
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
+import MyInjury from "./myInjuryButton";
+const NeckExercises = () => {
+    return ( 
+        <div>
+        <Navbar/>
+        <MyInjury/>
+
+        
+
+
+
+        <div className="exercise-container">
+        <header className="header">
+            <h1>Level 1</h1>
+        </header>
+        <div class="exercise-grid">
+            <div className="exercise-item">Exercise Video 1</div>
+            <div className="exercise-item">Exercise Video 2</div>
+            <div className="exercise-item">Exercise Video 3</div>
+        </div>
+        <div class="exercise-grid">
+            <div className="exercise-item">Exercise Video 4 </div>
+            <div className="exercise-item">Exercise Video 5</div>
+            <div className="exercise-item">Exercise Video 6</div>
+        </div>
+        </div> 
+        
+        <div className="too-easy-link-container">
+            <Link to="/neck-pain/level-2" className="too-easy-link">Too Easy</Link>
+        </div>
+        
+        </div> 
+
+        );
+}
+ 
+export default NeckExercises;
