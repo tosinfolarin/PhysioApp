3bef55c06015c489c0cf225830b3ddb131a66d3d
Theo <theo.osinfolarin@gmail.com>
2024-07-10 11:47:24 +0100

    red flags link + info updated

--- Frontend/src/components/RedFlagsButton.jsx
+++ Frontend/src/components/RedFlagsButton.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const RedFlags = () => {
     return (
       <div className="container">
-        <button className="button">
+        <Link to="/cad">
+        <button className="button"> 
           <h3 className="RedFlags"> Red Flags </h3>
         </button>
+        </Link>
       </div>
+      
     );
   };
   
--- Frontend/src/components/RedflagInfo.jsx
+++ Frontend/src/components/RedflagInfo.jsx
@@ -2,9 +2,11 @@ const RedFlagInfo = () => {
   return (
     <div className="red-flag-info-container">
       <h2>
-        Red flags are distinct indicators identified through medical history and clinical examination,
-        typically associated with a heightened risk of serious conditions such as infection, cancer, or fractures.
-        It is important these are not missed. Please check the tabs below before continuing with the exercises so that we can ensure these have not been missed.
+      MSK physiotherapists use a range of techniques to help patients manage pain, improve mobility, and enhance their overall physical function. 
+      Although a technique such as manual therapy are sometimes used, the bulk of NHS physiotherapy treatment is exercises, education, and advice. 
+      The purpose of this web application is to provide you with these three components without having to wait many months to receive the same end result. 
+      Prior to continuing with the exercises and advice, it is important that you familiarise yourself with the red flag information.
+      It is important that red flags are not missed. Please check the tabs below before continuing with the exercises.
       </h2>
     </div>
   );
