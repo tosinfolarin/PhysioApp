70cc788ddec49cd0dd3916d55f10c5328d9b19b0
Theo <theo.osinfolarin@gmail.com>
2024-07-20 08:36:28 +0100

    video uploads1

--- Frontend/src/components/NeckEx1.jsx
+++ Frontend/src/components/NeckEx1.jsx
@@ -15,15 +15,70 @@ const NeckExercises = () => {
         <header className="header">
             <h1>Level 1</h1>
         </header>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 1</div>
-            <div className="exercise-item">Exercise Video 2</div>
-            <div className="exercise-item">Exercise Video 3</div>
+        <div className="exercise-grid">
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/FjyYpcmbqWE"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/Fn_-utrs5Jo"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/SW47MU-d2cU"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
         </div>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 4 </div>
-            <div className="exercise-item">Exercise Video 5</div>
-            <div className="exercise-item">Exercise Video 6</div>
+        </div>
+        <div className="exercise-grid">
+            
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/dbTLdUXVv58"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/9TiI0_QaDIo"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/oCs3H41eBok"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
         </div>
         </div> 
         
--- Frontend/src/components/ShoulderEx1.jsx
+++ Frontend/src/components/ShoulderEx1.jsx
@@ -15,17 +15,73 @@ const ShoulderExercises = () => {
         <header className="header">
             <h1>Level 1</h1>
         </header>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 1</div>
-            <div className="exercise-item">Exercise Video 2</div>
-            <div className="exercise-item">Exercise Video 3</div>
+        <div className="exercise-grid">
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/n4gKZthb7uk"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/efjugnSo2PE"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/mnBBQBaomOc"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
         </div>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 4 </div>
-            <div className="exercise-item">Exercise Video 5</div>
-            <div className="exercise-item">Exercise Video 6</div>
+        </div>
+        <div className="exercise-grid">
+            
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/uPe_yl-JGWc"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/UALiBHvdjq8"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/ZNK9PRWfmhw"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
         </div>
         </div> 
+        
         
         <div className="too-easy-link-container">
             <Link to="/shoulder-pain/level-2" className="too-easy-link">Too Easy</Link>
