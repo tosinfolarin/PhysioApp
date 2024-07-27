c5d78417345bab87ce8608b03e3686d9f6d8eca8
Theo <theo.osinfolarin@gmail.com>
2024-07-27 11:37:43 +0100

    backend progress1

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,38 @@
 // Import express.js
 const express = require("express");
+const mysql = require('mysql')
+const cors = require('cors')
 
 // Create express app
-var app = express();
+const app = express();
+
+app.use(cors())
+
+// creating a variable for the mySQL database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"",
+    database:"PhysioApp"
+
+})
+
+
+app.get("/", (req, res)=> {
+    return res.json("From backend side")
+})
+
+//API to get information from the database
+
+app.get("/Patients", (req, res)=> {
+    const sql = "SELECT * FROM Patients";
+    db.query(sql, (err, data)=> {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+
+app.listen(8080, () => {
+    console.log("listening");
+})
