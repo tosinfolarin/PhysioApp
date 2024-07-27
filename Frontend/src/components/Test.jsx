ff4713685b94cec801e2c22c57176a51e0ad94eb
Theo <theo.osinfolarin@gmail.com>
2024-07-27 12:28:19 +0100

    pulling data from backend to frontend in a table

--- Frontend/src/components/Test.jsx
+++ Frontend/src/components/Test.jsx
@@ -1,17 +1,57 @@
 import { useEffect } from "react";
+import {useState} from "react";
 
 const Test = () => {
+
+    const [data, setData] = useState([])
+
+
 //useEffect hook to fetch the data from the route the data is stored in
     useEffect (()=> {
         fetch('http://localhost:8080/Patients')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setData(data))
         .catch(err => console.log(err));
 
     }, [])
     return (
-        <div>
-            <h1> Patients contents </h1>
+        <div className="Test-Data">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Last Name</th>
+                <th>Preferred Name</th>
+                <th>DOB</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>PainScore</th>
+                <th>ReadInfo</th>
+                <th>PMH</th>
+                <th>Notes</th>
+                <th>Symptom Area</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((data, i) => (
+                <tr key={i}>
+                  <td>{data.PatientID}</td>
+                  <td>{data.FirstName}</td>
+                  <td>{data.LastName}</td>
+                  <td>{data.PreferredName}</td>
+                  <td>{data.DOB}</td>
+                  <td>{data.Email}</td>
+                  <td>{data.PhoneNo}</td>
+                  <td>{data.PainScore}</td>
+                  <td>{data.ReadInfo}</td>
+                  <td>{data.PMH}</td>
+                  <td>{data.Notes}</td>
+                  <td>{data.SymptomArea}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       );
 }
