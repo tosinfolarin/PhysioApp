b88e1de8c829acb8b9dcbeb700d8880c9de2d3dc
Theo <theo.osinfolarin@gmail.com>
2024-08-07 11:23:24 +0100

    knee exercises 1

--- Frontend/src/components/KneeEx1.jsx
+++ Frontend/src/components/KneeEx1.jsx
@@ -15,15 +15,70 @@ const KneeExercises = () => {
         <header className="header">
             <h1>Level 1</h1>
         </header>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 1</div>
-            <div className="exercise-item">Exercise Video 2</div>
-            <div className="exercise-item">Exercise Video 3</div>
+        <div className="exercise-grid">
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/KcNl9yLtcKo"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/ROgrgQLXlM4"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/jS4htCU5xMM"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
         </div>
-        <div class="exercise-grid">
-            <div className="exercise-item">Exercise Video 4 </div>
-            <div className="exercise-item">Exercise Video 5</div>
-            <div className="exercise-item">Exercise Video 6</div>
+        </div>
+        <div className="exercise-grid">
+            
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/qo-e_CqkYMU"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/UOYvAsEdLVM"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
+        <div className="exercise-item">
+        <iframe
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/6_NYr7Sheuw"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+        </div>
         </div>
         </div> 
         
