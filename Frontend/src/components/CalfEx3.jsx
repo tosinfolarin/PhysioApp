eb7f4c7c7b78baeb173d24eceada93394ae60221
Theo <theo.osinfolarin@gmail.com>
2024-08-23 09:49:18 +0100

    calf exercises

--- Frontend/src/components/CalfEx3.jsx
+++ Frontend/src/components/CalfEx3.jsx
@@ -40,7 +40,7 @@ const CalfExercises3 = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src="3"
+                            src="https://www.youtube.com/embed/2IOVr0b26pU"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
