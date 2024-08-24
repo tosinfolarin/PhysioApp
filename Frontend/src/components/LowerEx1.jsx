6c619737d489568ce95244a732e2e3b1656f1099
Theo <theo.osinfolarin@gmail.com>
2024-08-24 12:21:25 +0100

    sciatic exercise added

--- Frontend/src/components/LowerEx1.jsx
+++ Frontend/src/components/LowerEx1.jsx
@@ -13,14 +13,14 @@ const LowerExercises = () => {
 
         <div className="exercise-container">
         <header className="header">
-            <h1>Level 1</h1>
+            <h1>Sciatica</h1>
         </header>
         <div className="exercise-grid">
         <div className="exercise-item">
         <iframe
                             width="100%"
                             height="100%"
-                            src="https://www.youtube.com/embed/McU92uFu8j0"
+                            src="https://www.youtube.com/embed/-LlGOh0Q9dM"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
@@ -30,7 +30,7 @@ const LowerExercises = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src=""
+                            src="https://www.youtube.com/embed/3SyzSl6Oj-Y"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
@@ -40,7 +40,7 @@ const LowerExercises = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src=""
+                            src="https://www.youtube.com/embed/PdEKzbBFZrE"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
@@ -53,7 +53,7 @@ const LowerExercises = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src=""
+                            src="https://www.youtube.com/embed/2YyL9jtQ07s"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
@@ -63,7 +63,7 @@ const LowerExercises = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src=""
+                            src="https://www.youtube.com/embed/rW22RnECiLk"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
@@ -73,7 +73,7 @@ const LowerExercises = () => {
         <iframe
                             width="100%"
                             height="100%"
-                            src=""
+                            src="https://www.youtube.com/embed/NLGduAw_hB8"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
